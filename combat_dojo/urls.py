226c126a7c8c560ad71a6b594af8ffcd885c9042
Gary Holler <gholler76@gmail.com>
2021-04-13 09:27:19 -0400

    deploy fixes

--- combat_dojo/urls.py
+++ combat_dojo/urls.py
@@ -1,12 +1,7 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-
-if settings.DEBUG:
-    import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('combat_app.routes')),
-    path('__debug__/', include(debug_toolbar.urls)),
 ]
